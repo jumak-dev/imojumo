50096da45655f795dd63ad1c22359ad0de626244
Jung Su <wjdwjdtn92@naver.com>
2023-03-04 17:03:58 +0900

    💄 피그마에 맞게 css 상수 변경 - #1

--- src/styles/variables.tsx
+++ src/styles/variables.tsx
@@ -4,30 +4,23 @@ const variables = css`
   :root {
     --black: #000;
     --white: #fff;
+    -—color-primary-mint: #59c1bd;
+    -—color-primary-pink: #f67474;
+    -—color-heart: #ff3c3c;
+    -—color-content-text: #232629;
+    -—color-placeholder: #b2b3b9;
+    -—color-borderbox-line: #727272;
+    -—color-inputbox-line: #d0d0d0;
+    -—color-inputbox-bg: #efefef;
+    -—color-borderbottom-color: #bdbdbd;
+    -—color-subtitle-bg-color: #ff3c3c;
 
-    --font-size-xss: 12px;
-    --line-hieght-xss: 16px;
-    --letter-spacing-xss: -0.005em;
-
-    --font-size-xs: 13px;
-    --line-hieght-xs: 20px;
-    --letter-spacing-xs: -0.01em;
-
-    --font-size-sm: 14px;
-    --line-hieght-sm: 24px;
-    --letter-spacing-sm: -0.01em;
-
-    --font-size-md: 16px;
-    --line-hieght-md: 24px;
-    --letter-spacing-md: -0.01em;
-
-    --font-size-lg: 18px;
-    --line-hieght-lg: 28px;
-    --letter-spacing-lg: -0.02em;
-
+    --font-size-xxl: 30px;
     --font-size-xl: 24px;
-    --line-hieght-xl: 34px;
-    --letter-spacing-xl: -0.01em;
+    --font-size-l: 20px;
+    --font-size-m: 16px;
+    --font-size-sm: 13px;
+    --font-size-xs: 10px;
   }
 `;
 
