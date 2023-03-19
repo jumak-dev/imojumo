2514de419fdec7492388bd4a0f5f410d62e15e3d
youa7878 <dkgus7123@naver.com>
2023-03-19 14:20:00 +0900

    💄 shared css 추가 - #31

---
+++ src/styles/shared.tsx
@@ -0,0 +1,29 @@
+import { css } from 'styled-components';
+
+export const Flex = css`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+export const RowFlex = css`
+  display: flex;
+  flex-direction: row;
+`;
+
+export const RowFlexCenter = css`
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+`;
+
+export const ColFlex = css`
+  display: flex;
+  flex-direction: column;
+`;
+
+export const ColFlexCenter = css`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+`;
