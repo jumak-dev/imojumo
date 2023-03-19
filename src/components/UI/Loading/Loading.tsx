2a22f6336324f35dea58631a478e8b4b205c5fce
Paksubeen <tnqls520@gmail.com>
2023-03-19 15:08:32 +0900

    🔨 Animation keyframes 분리

--- src/components/UI/Loading/Loading.tsx
+++ src/components/UI/Loading/Loading.tsx
@@ -1,4 +1,4 @@
-import styled from 'styled-components';
+import styled, { keyframes } from 'styled-components';
 
 function Loading() {
   return (
@@ -15,6 +15,12 @@ const Loader = styled.div`
   left: calc(50% - 20px);
 `;
 
+const shadow = keyframes`
+  50% {
+    transform: scale(1.2, 1);
+  }
+`;
+
 const Shadow = styled.div`
   width: 50px;
   height: 5px;
@@ -24,12 +30,25 @@ const Shadow = styled.div`
   top: 60px;
   left: 0;
   border-radius: 50%;
-  animation: shadow 0.5s linear infinite;
+  animation: ${shadow} 0.5s linear infinite;
+`;
 
-  @keyframes shadow {
-    50% {
-      transform: scale(1.2, 1);
-    }
+const animate = keyframes`
+  17% {
+    border-bottom-right-radius: 3px;
+  }
+  25% {
+    transform: translateY(9px) rotate(22.5deg);
+  }
+  50% {
+    transform: translateY(18px) scale(1, 0.9) rotate(45deg);
+    border-bottom-right-radius: 40px;
+  }
+  75% {
+    transform: translateY(9px) rotate(67.5deg);
+  }
+  100% {
+    transform: translateY(0) rotate(90deg);
   }
 `;
 
@@ -37,30 +56,11 @@ const Box = styled.div`
   width: 50px;
   height: 50px;
   background: var(--color-primary-mint);
-  animation: animate 0.5s linear infinite;
+  animation: ${animate} 0.5s linear infinite;
   position: absolute;
   top: 0;
   left: 0;
   border-radius: 3px;
-
-  @keyframes animate {
-    17% {
-      border-bottom-right-radius: 3px;
-    }
-    25% {
-      transform: translateY(9px) rotate(22.5deg);
-    }
-    50% {
-      transform: translateY(18px) scale(1, 0.9) rotate(45deg);
-      border-bottom-right-radius: 40px;
-    }
-    75% {
-      transform: translateY(9px) rotate(67.5deg);
-    }
-    100% {
-      transform: translateY(0) rotate(90deg);
-    }
-  }
 `;
 
 export default Loading;
