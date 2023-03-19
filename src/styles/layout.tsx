ad349f59f8286cb3bbddca411d24424c49031dd4
youa7878 <dkgus7123@naver.com>
2023-03-19 14:19:21 +0900

    💄 main container layout 추가 - #31

---
+++ src/styles/layout.tsx
@@ -0,0 +1,11 @@
+import styled from 'styled-components';
+
+// 최상위 컨테이너
+const MainContainer = styled.main`
+  max-width: 1120px;
+  width: 100%;
+  margin: auto;
+  background-color: blue;
+`;
+
+export default MainContainer;
