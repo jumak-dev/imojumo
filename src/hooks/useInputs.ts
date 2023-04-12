a13bc5ea9ec3b3478f3475b8c3b558a1927effbd
Jung Su <wjdwjdtn92@naver.com>
2023-04-12 22:24:29 +0900

    ✨ useInputs 커스텀 hooks 추가 - #41

---
+++ src/hooks/useInputs.ts
@@ -0,0 +1,18 @@
+import React, { useState, useCallback } from 'react';
+
+function useInputs<T extends Record<string, any>>(initialForm: T) {
+  const [form, setForm] = useState<T>(initialForm);
+  const reset = useCallback(() => {
+    setForm(initialForm);
+  }, [initialForm]);
+
+  const onChange = useCallback((event: React.ChangeEvent<HTMLInputElement>) => {
+    const { name, value } = event.target;
+
+    setForm((preState) => ({ ...preState, [name]: value }));
+  }, []);
+
+  return [form, onChange, reset] as const;
+}
+
+export default useInputs;
