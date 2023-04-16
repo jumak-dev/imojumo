b0f1a56b083abed9c0c43e48080bc0b479902ce7
LeeHyoGeun-create <dkdkdk99835@gmail.com>
2023-04-16 23:05:47 +0900

    :hammer: useId 대체

--- src/components/UI/Breadcrumb/Breadcrumb.tsx
+++ src/components/UI/Breadcrumb/Breadcrumb.tsx
@@ -1,18 +1,19 @@
-import { useId } from 'react';
 import { Link } from 'react-router-dom';
 import styled from 'styled-components';
 
 const dummyData = [
-  { path: '#', title: '독서토론' },
-  { path: '#', title: '미드나잇 라이브러리' },
-  { path: '#', title: '미드나잇 라이브러리' },
+  { id: 0, path: '#', title: '독서토론' },
+  { id: 1, path: '#', title: '미드나잇 라이브러리' },
+  { id: 2, path: '#', title: '미드나잇 라이브러리' },
 ];
 
 interface BreadcrumbItem {
+  id: number;
   path: string;
   title: string;
 }
 
+// 현재 dummyData를 쓰기 위해 option으로 해 놓았지만 추후 제거할 예정
 interface BreadcrumbProps {
   items?: BreadcrumbItem[];
 }
@@ -21,7 +22,7 @@ function Breadcrumb({ items = dummyData }: BreadcrumbProps) {
   return (
     <Ol>
       {items.map((item, index) => (
-        <li key={useId()}>
+        <li key={item.id}>
           {index !== items.length - 1 ? (
             <Link to={item.path}>{item.title}</Link>
           ) : (
