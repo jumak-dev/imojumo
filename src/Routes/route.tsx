33c23c0b2d6175bca769bcb5cdfc93fb364d78db
youa7878 <dkgus7123@naver.com>
2023-04-17 15:44:46 +0900

    🔨 route Home 추가 - #45

--- src/Routes/route.tsx
+++ src/Routes/route.tsx
@@ -1,12 +1,26 @@
 import { Outlet } from 'react-router-dom';
 import GlobalStyle from '../styles/GlobalStyle';
 import Footer from '../components/Layout/Footer/Footer';
+import Banner from '../components/Main/Banner';
 
 // Header, Footer가 들어간 Page Root
 export function Root() {
   return (
     <>
       <GlobalStyle />
+      {/* Header 추후에 추가 */}
+      <Outlet />
+      <Footer />
+    </>
+  );
+}
+
+// Header, Footer, Banner가 들어간 Page Root
+export function Home() {
+  return (
+    <>
+      <GlobalStyle />
+      <Banner />
       {/* Header 추후에 추가 */}
       <Outlet />
       <Footer />
