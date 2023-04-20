c02e8a0dd28c08aabeb00a698149ae93c16a0c47
Jung Su <wjdwjdtn92@naver.com>
2023-04-20 21:58:31 +0900

    🔨 토론 종류에 따른 존거부 렌더링 추가 - #41

--- src/pages/PostNewPage.tsx
+++ src/pages/PostNewPage.tsx
@@ -1,15 +1,49 @@
 import React from 'react';
-import PostForm from '../components/PostForm';
+import styled from 'styled-components';
+import BookDiscussionFrom from '../components/PostNew/BookDiscussionFrom';
+import DiscussionTab from '../components/PostNew/DiscussionTab';
+import ProConDiscussionFrom from '../components/PostNew/ProConDiscussionFrom';
 import MainContainer from '../styles/layout';
+import { Flex, ScreenReaderTextCSS } from '../styles/shared';
 
-function PostNewPage() {
+interface PostNewPageProps {
+  discussionType: 'proCon' | 'book';
+}
+
+function PostNewPage({ discussionType }: PostNewPageProps) {
+  const title = {
+    proCon: '찬반 토론 게신글 작성하기',
+    book: '독서 토론 게신글 작성하기',
+  }[discussionType];
+
   const handleSubmit = () => {};
 
   return (
     <MainContainer>
-      <PostForm onSubmit={handleSubmit} />
+      <PostNewPageContainer>
+        <PostNewPageTitle>{title}</PostNewPageTitle>
+        <DiscussionTab />
+        {
+          {
+            proCon: <ProConDiscussionFrom onSubmit={handleSubmit} />,
+            book: <BookDiscussionFrom onSubmit={handleSubmit} />,
+          }[discussionType]
+        }
+      </PostNewPageContainer>
     </MainContainer>
   );
 }
+
+const PostNewPageContainer = styled.article`
+  ${Flex}
+  width: 100%;
+  flex-direction: column;
+  gap: 48px;
+  padding: 48px 16px;
+`;
+
+const PostNewPageTitle = styled.h2`
+  ${ScreenReaderTextCSS};
+`;
 
 export default PostNewPage;
