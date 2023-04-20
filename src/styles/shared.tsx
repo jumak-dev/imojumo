0cd117ff8cd48e38aa83f0894b116aedc0291c2c
Jung Su <wjdwjdtn92@naver.com>
2023-04-20 21:47:36 +0900

    💄 ScreenReaderTextCSS 추가 - #31

--- src/styles/shared.tsx
+++ src/styles/shared.tsx
@@ -48,3 +48,16 @@ export const InputCSS = css`
     color: var(--color-placeholder);
   }
 `;
+
+export const ScreenReaderTextCSS = css`
+  border: 0;
+  clip: rect(1px, 1px, 1px, 1px);
+  clip-path: inset(50%);
+  height: 1px;
+  margin: -1px;
+  overflow: hidden;
+  padding: 0;
+  position: absolute;
+  width: 1px;
+  word-wrap: normal !important;
+`;
