d5cc16942b53e88748b13a29c12a8d05dc1ae024
Jung Su <wjdwjdtn92@naver.com>
2023-04-20 17:46:56 +0900

    ✨ SearchBook 컴포넌트 분리 생성 - #41

---
+++ src/components/PostNew/SearchBook.tsx
@@ -0,0 +1,81 @@
+import React from 'react';
+import styled from 'styled-components';
+import useInputs from '../../hooks/useInputs';
+import { ColFlexCenter } from '../../styles/shared';
+import { ButtonBox } from '../UI/Button/Button';
+import { Card } from '../UI/Card/Card';
+import Input from '../UI/Input/Input';
+
+interface SearchBookProps {
+  onSearch: (bookTitle: string) => void;
+}
+
+function SearchBook({ onSearch }: SearchBookProps) {
+  const [{ bookTitle }, onChange] = useInputs({ bookTitle: '' });
+
+  const handleSearchClick = () => {
+    onSearch(bookTitle);
+  };
+  return (
+    <SerachBookContainer>
+      <SearchBookTitle>토론 도서 선택하기</SearchBookTitle>
+      <SearchBookInputContainer>
+        <BookTitleInput
+          value={bookTitle}
+          name="bookTitle"
+          placeholder="도서명을..."
+          onChange={onChange}
+        />
+        <BookSearchButton
+          type="button"
+          buttonType="buttonRight"
+          buttonColor="white"
+          buttonSize="sm"
+          onClick={handleSearchClick}
+        >
+          찾기
+        </BookSearchButton>
+      </SearchBookInputContainer>
+    </SerachBookContainer>
+  );
+}
+
+const SerachBookContainer = styled.section`
+  width: 100%;
+  height: 171px;
+  align-items: center;
+  gap: 16px;
+
+  ${Card}
+  ${ColFlexCenter}
+  padding: 32px;
+`;
+
+const SearchBookTitle = styled.h3`
+  width: 100%;
+  text-align: center;
+  font-weight: 700;
+  font-size: var(--font-size-xl);
+  line-height: 29px;
+  letter-spacing: -0.02em;
+  color: #1d1d1b;
+`;
+
+const SearchBookInputContainer = styled.div`
+  display: flex;
+  width: 100%;
+`;
+
+const BookTitleInput = styled(Input)`
+  flex: 1 1 0;
+  border-radius: 5px 0 0 5px;
+  border-right: 0;
+`;
+
+const BookSearchButton = styled(ButtonBox)`
+  width: 74px;
+  height: 50px;
+  color: var(--black);
+`;
+
+export default SearchBook;
