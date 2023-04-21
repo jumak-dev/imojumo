9c7fac61ee7ad615f617a038481660ffb33f2137
Paksubeen <tnqls520@gmail.com>
2023-04-21 16:00:19 +0900

    💄 찬반토론(상세) 화면 CSS 구현 - #37

--- src/pages/ProConDiscussionDetailPage.tsx
+++ src/pages/ProConDiscussionDetailPage.tsx
@@ -1,7 +1,48 @@
-import React from 'react';
+import styled from 'styled-components';
+import { BsChatLeftDots, BsInfoCircle } from 'react-icons/bs';
+import MainContainer from '../styles/layout';
+import { AlignCenter } from '../styles/shared';
+import DiscussionInformation from '../components/ProConDiscussionDetail/DiscussionInformation';
+import TopicDescription from '../components/ProConDiscussionDetail/TopicDescription';
+import CommentForm from '../components/Comment/CommentForm';
+import CommentList from '../components/Comment/CommentList';
 
 function ProConDiscussionDetailPage() {
-  return <div>ProConDiscussionDetailPage</div>;
+  return (
+    <MainContainer>
+      <DiscussionInformation />
+      <SubtitleBox>
+        <Subtitle>
+          주제 설명 <BsInfoCircle />
+        </Subtitle>
+      </SubtitleBox>
+      <TopicDescription />
+      <SubtitleBox>
+        <Subtitle>
+          참여하기 <BsChatLeftDots />
+        </Subtitle>
+      </SubtitleBox>
+      <CommentForm />
+      <CommentList />
+    </MainContainer>
+  );
 }
+
+const SubtitleBox = styled.div`
+  ${AlignCenter}
+  justify-content: space-between;
+  height: 50px;
+  padding: 0 24px;
+  border-radius: 8px;
+  background-color: var(--color-subtitle-bg-color);
+  box-shadow: rgba(99, 99, 99, 0.2) 0px 2px 8px 0px;
+`;
+
+const Subtitle = styled.h3`
+  ${AlignCenter}
+  gap: 8px;
+  font-weight: bold;
+  font-size: var(--font-size-l);
+`;
 
 export default ProConDiscussionDetailPage;
