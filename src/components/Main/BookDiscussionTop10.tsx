dc00267adc35f02e1c7d45286b9084fbd5820d16
youa7878 <dkgus7123@naver.com>
2023-04-24 15:35:13 +0900

    🔨 import 수정 - #34

--- src/components/Main/BookDiscussionTop10.tsx
+++ src/components/Main/BookDiscussionTop10.tsx
@@ -1,9 +1,9 @@
 import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
-import styled, { css } from 'styled-components';
-import { FiHeart } from 'react-icons/fi';
-import { FaHeart } from 'react-icons/fa';
+import styled from 'styled-components';
 import { Card } from '../UI/Card/Card';
+import LikeIcon from '../UI/Icon/LikeIcon';
+import UnlikeIcon from '../UI/Icon/UnlikeIcon';
 import { ColFlex, Flex, truncateTextCSS } from '../../styles/shared';
 
 function BookDiscussionTop10() {
@@ -62,32 +62,6 @@ const CardTitle = styled.p`
   margin: 5px 10px;
   font-weight: bold;
   font-size: var(--font-size-m);
-`;
-
-const iconCSS = css`
-  position: absolute;
-  font-size: 25px;
-  right: 13px;
-  top: 15px;
-  filter: drop-shadow(0 1px 1px rgba(0, 0, 0, 0.6));
-`;
-
-export const UnlikeIcon = styled(FiHeart)`
-  ${iconCSS}
-  color: var(--white);
-
-  &:hover {
-    color: var(--color-heart);
-  }
-`;
-
-export const LikeIcon = styled(FaHeart)`
-  ${iconCSS}
-  color: var(--color-heart);
-
-  &:hover {
-    color: #ff1c1c;
-  }
 `;
 
 export default BookDiscussionTop10;
