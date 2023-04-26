d7c65ad4361e1f8474cae95053f0e51e001d6f45
youa7878 <dkgus7123@naver.com>
2023-04-26 23:31:30 +0900

    🔨 찬반토론 카드 컴포넌트 적용 - #77

--- src/pages/SearchPage.tsx
+++ src/pages/SearchPage.tsx
@@ -1,7 +1,22 @@
-import React from 'react';
+import styled from 'styled-components';
+import ProConDiscussionSearchCard from '../components/Search/ProConDiscussionSearchCard';
+import MainContainer from '../styles/layout';
+import { Flex } from '../styles/shared';
 
 function SearchPage() {
-  return <div>SearchPage</div>;
+  return (
+    <MainContainer>
+      <ProConDiscussionSearchCardContainer>
+        <ProConDiscussionSearchCard />
+        <ProConDiscussionSearchCard />
+        <ProConDiscussionSearchCard />
+      </ProConDiscussionSearchCardContainer>
+    </MainContainer>
+  );
 }
+
+const ProConDiscussionSearchCardContainer = styled.section`
+  ${Flex}
+`;
 
 export default SearchPage;
