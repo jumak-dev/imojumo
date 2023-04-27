cb998046f10ae236f5b68d1b376ae69588aef0dc
youa7878 <dkgus7123@naver.com>
2023-04-27 15:44:30 +0900

    🔨 SearchNav 추가 - #77

--- src/pages/SearchPage.tsx
+++ src/pages/SearchPage.tsx
@@ -1,17 +1,24 @@
+import { useState } from 'react';
 import styled from 'styled-components';
 import ProConDiscussionSearchCard from '../components/Search/ProConDiscussionSearchCard';
 import MainContainer from '../styles/layout';
+import SearchNav from '../components/Search/SearchNav';
 import { Flex } from '../styles/shared';
 
 function SearchPage() {
+  const [currentTap, setCurrentTap] = useState('Search All');
+
   return (
-    <MainContainer>
-      <ProConDiscussionSearchCardContainer>
-        <ProConDiscussionSearchCard />
-        <ProConDiscussionSearchCard />
-        <ProConDiscussionSearchCard />
-      </ProConDiscussionSearchCardContainer>
-    </MainContainer>
+    <>
+      <SearchNav currentTap={currentTap} setCurrentTap={setCurrentTap} />
+      <MainContainer>
+        <ProConDiscussionSearchCardContainer>
+          <ProConDiscussionSearchCard />
+          <ProConDiscussionSearchCard />
+          <ProConDiscussionSearchCard />
+        </ProConDiscussionSearchCardContainer>
+      </MainContainer>
+    </>
   );
 }
 
