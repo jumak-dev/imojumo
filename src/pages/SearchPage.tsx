29404a5bc3c80cda4c86fd5c7eff0336a88b1cc3
youa7878 <dkgus7123@naver.com>
2023-04-27 17:01:19 +0900

    🔨 SubtitleSection 추가 - #77

--- src/pages/SearchPage.tsx
+++ src/pages/SearchPage.tsx
@@ -3,6 +3,7 @@ import styled from 'styled-components';
 import ProConDiscussionSearchCard from '../components/Search/ProConDiscussionSearchCard';
 import MainContainer from '../styles/layout';
 import SearchNav from '../components/Search/SearchNav';
+import SubtitleSection from '../components/Search/SubtitleSection';
 import { Flex } from '../styles/shared';
 
 function SearchPage() {
@@ -12,6 +13,16 @@ function SearchPage() {
     <>
       <SearchNav currentTap={currentTap} setCurrentTap={setCurrentTap} />
       <MainContainer>
+        <SubtitleSection
+          subtitle="독서토론"
+          postCount={1234}
+          setCurrentTap={setCurrentTap}
+        />
+        <SubtitleSection
+          subtitle="찬반토론"
+          postCount={4321}
+          setCurrentTap={setCurrentTap}
+        />
         <ProConDiscussionSearchCardContainer>
           <ProConDiscussionSearchCard />
           <ProConDiscussionSearchCard />
