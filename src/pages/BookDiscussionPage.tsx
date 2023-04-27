fdcd391e5dac66a6bcb453398bc3a385ec2c435e
youa7878 <dkgus7123@naver.com>
2023-04-27 19:12:14 +0900

    🔨 import 수정 - #77

--- src/pages/BookDiscussionPage.tsx
+++ src/pages/BookDiscussionPage.tsx
@@ -2,138 +2,14 @@ import { useState } from 'react';
 import styled from 'styled-components';
 import MainContainer from '../styles/layout';
 import BookDiscussionCard from '../components/BookDiscussion/BookDiscussionCard';
-import Pagination, { PageInfo } from '../components/UI/Pagination/Pagination';
-
-export interface BookDiscussionInfo {
-  id: string;
-  author: string;
-  title: string;
-  content: string;
-  like: number;
-  createdAt: string;
-  updatedAt: string;
-}
-
-interface BookDiscussionData {
-  pageInfo: PageInfo;
-  data: BookDiscussionInfo[];
-}
-
-const dummyData: BookDiscussionData = {
-  pageInfo: {
-    page: 1,
-    totalPage: 10,
-    totalCount: 100,
-    currentCount: 10,
-  },
-  data: [
-    {
-      id: '1',
-      author: 'wjdwjdtn92',
-      title: '미드나잇 라이브러리',
-      content:
-        '랄라라라라라라라라라라라랄랄랄랄라라라라라라라라라라라랄랄랄랄라라라라라라라라라라라랄랄랄랄라라라라라라라라라라라랄랄랄랄라라라라라라라라라라라랄랄랄랄라라라라라라라라라라라랄랄랄',
-      like: 10,
-      createdAt: '2023.02.03 18:51:09 GMT+0900',
-      updatedAt: '2023.02.03 18:51:09 GMT+0900',
-    },
-    {
-      id: '2',
-      author: 'yua77',
-      title: '미드나잇 라이브러리',
-      content: '랄라라라라라라라라라라라랄랄랄',
-      like: 10,
-      createdAt: '2023.02.03 18:51:09 GMT+0900',
-      updatedAt: '2023.02.03 18:51:09 GMT+0900',
-    },
-    {
-      id: '3',
-      author: 'yua77',
-      title: '미드나잇 라이브러리',
-      content: '랄라라라라라라라라라라라랄랄랄',
-      like: 10,
-      createdAt: '2023.02.03 18:51:09 GMT+0900',
-      updatedAt: '2023.02.03 18:51:09 GMT+0900',
-    },
-    {
-      id: '4',
-      author: 'yua77',
-      title: '미드나잇 라이브러리',
-      content: '랄라라라라라라라라라라라랄랄랄',
-      like: 10,
-      createdAt: '2023.02.03 18:51:09 GMT+0900',
-      updatedAt: '2023.02.03 18:51:09 GMT+0900',
-    },
-    {
-      id: '5',
-      author: 'yua77',
-      title: '미드나잇 라이브러리',
-      content: '랄라라라라라라라라라라라랄랄랄',
-      like: 10,
-      createdAt: '2023.02.03 18:51:09 GMT+0900',
-      updatedAt: '2023.02.03 18:51:09 GMT+0900',
-    },
-    {
-      id: '6',
-      author: 'yua77',
-      title: '미드나잇 라이브러리',
-      content: '랄라라라라라라라라라라라랄랄랄',
-      like: 10,
-      createdAt: '2023.02.03 18:51:09 GMT+0900',
-      updatedAt: '2023.02.03 18:51:09 GMT+0900',
-    },
-    {
-      id: '7',
-      author: 'yua77',
-      title: '미드나잇 라이브러리',
-      content: '랄라라라라라라라라라라라랄랄랄',
-      like: 10,
-      createdAt: '2023.02.03 18:51:09 GMT+0900',
-      updatedAt: '2023.02.03 18:51:09 GMT+0900',
-    },
-    {
-      id: '8',
-      author: 'yua77',
-      title: '미드나잇 라이브러리',
-      content: '랄라라라라라라라라라라라랄랄랄',
-      like: 10,
-      createdAt: '2023.02.03 18:51:09 GMT+0900',
-      updatedAt: '2023.02.03 18:51:09 GMT+0900',
-    },
-    {
-      id: '9',
-      author: 'yua77',
-      title: '미드나잇',
-      content: '랄라라라라라라라라라라라랄랄랄',
-      like: 10,
-      createdAt: '2023.02.03 18:51:09 GMT+0900',
-      updatedAt: '2023.02.03 18:51:09 GMT+0900',
-    },
-    {
-      id: '10',
-      author: 'yua77',
-      title: '미드나잇',
-      content: '랄라라라라라라라라라라라랄랄랄',
-      like: 10,
-      createdAt: '2023.02.03 18:51:09 GMT+0900',
-      updatedAt: '2023.02.03 18:51:09 GMT+0900',
-    },
-    {
-      id: '11',
-      author: 'yua77',
-      title: '미드나잇',
-      content: '랄라라라라라라라라라라라랄랄랄',
-      like: 10,
-      createdAt: '2023.02.03 18:51:09 GMT+0900',
-      updatedAt: '2023.02.03 18:51:09 GMT+0900',
-    },
-  ],
-};
+import Pagination from '../components/UI/Pagination/Pagination';
+import { BookDiscussionInfo, PageInfo } from '../types';
+import BOOKDISCUSSION_DUMMY from '../components/BookDiscussion/BOOKDISCUSSION_DUMMY';
 
 function BookDiscussion() {
-  const [posts] = useState<BookDiscussionInfo[]>(dummyData.data);
+  const [posts] = useState<BookDiscussionInfo[]>(BOOKDISCUSSION_DUMMY.data);
   const [paginate, setPaginate] = useState(1);
-  const [paginationInfo] = useState<PageInfo>(dummyData.pageInfo);
+  const [paginationInfo] = useState<PageInfo>(BOOKDISCUSSION_DUMMY.pageInfo);
 
   /* 9개씩 가지고 옴
   useEffect(() => {
