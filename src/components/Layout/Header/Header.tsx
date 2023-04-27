f5eb08ad2298c25bf938a5e9df5a09288ed73178
Paksubeen <tnqls520@gmail.com>
2023-04-27 14:56:56 +0900

    💄 Header 컴포넌트 CSS 구현 - #80

---
+++ src/components/Layout/Header/Header.tsx
@@ -0,0 +1,160 @@
+import { Link, NavLink, useNavigate } from 'react-router-dom';
+import styled from 'styled-components';
+import { AiFillHome, AiOutlineSearch } from 'react-icons/ai';
+import { BsBell, BsDot, BsPersonCircle } from 'react-icons/bs';
+import Button from '../../UI/Button/Button';
+import { AlignCenter, RowFlex, RowFlexCenter } from '../../../styles/shared';
+
+function Header() {
+  const user = true;
+  const navigate = useNavigate();
+
+  const handleClick = () => {
+    navigate('/posts/new');
+  };
+
+  return (
+    <HeaderLayout>
+      <HeaderContainer>
+        <HomeLink to="/">
+          <AiFillHome size={32} />
+          <LogoTitle>이모저모</LogoTitle>
+        </HomeLink>
+        <SearchContainer>
+          <SearchIcon />
+          <SearchInput type="search" placeholder="검색어를 입력하세요" />
+        </SearchContainer>
+        <NavContainer>
+          <NavLinkList>
+            <NavLinkItem>
+              <NavLink to="/book-discussion">독서토론</NavLink>
+            </NavLinkItem>
+            <NavLinkItem>
+              <NavLink to="/pro-con-discussion">찬반토론</NavLink>
+            </NavLinkItem>
+            <NavLinkItem>
+              <NavLink to="/likes">찜 목록</NavLink>
+            </NavLinkItem>
+          </NavLinkList>
+        </NavContainer>
+        <Button
+          type="button"
+          buttonType="button"
+          buttonColor="mint"
+          buttonSize="sm"
+          onClick={handleClick}
+        >
+          토론하기
+        </Button>
+        {user ? (
+          <ButtonContainer>
+            <AlarmIcon />
+            <Nickname>유아유아짱</Nickname>
+            <UserIcon />
+          </ButtonContainer>
+        ) : (
+          <ButtonContainer>
+            <TabLink to="/login">로그인</TabLink>
+            <DotIcon />
+            <TabLink to="/signup">회원가입</TabLink>
+          </ButtonContainer>
+        )}
+      </HeaderContainer>
+    </HeaderLayout>
+  );
+}
+
+const HeaderLayout = styled.header`
+  ${RowFlexCenter}
+  width: 100%;
+  height: 80px;
+  border-bottom: 1px solid var(--color-borderbottom-color);
+`;
+
+const HeaderContainer = styled.div`
+  ${AlignCenter}
+  justify-content: space-between;
+  width: 1120px;
+  gap: 16px;
+  white-space: nowrap;
+`;
+
+const HomeLink = styled(Link)`
+  ${AlignCenter}
+`;
+
+const LogoTitle = styled.h1`
+  margin-left: 4px;
+  font-weight: 700;
+  font-size: var(--font-size-xl);
+`;
+
+const SearchContainer = styled.div`
+  ${AlignCenter}
+  width: 440px;
+  height: 40px;
+  padding: 0 8px;
+  border: 1px solid var(--color-inputbox-line);
+  border-radius: 8px;
+`;
+
+const SearchIcon = styled(AiOutlineSearch)`
+  width: 20px;
+  height: 20px;
+  margin-right: 4px;
+  color: var(--color-inputbox-line);
+`;
+
+const SearchInput = styled.input`
+  flex: 1;
+  padding: 0;
+  font-size: var(--font-size-sm);
+`;
+
+const NavContainer = styled.nav`
+  ${RowFlex}
+`;
+
+const NavLinkList = styled.ul`
+  ${RowFlex}
+  gap: 16px;
+`;
+
+const NavLinkItem = styled.li`
+  font-size: var(--font-size-l);
+
+  & > a.active {
+    font-weight: 600;
+  }
+`;
+
+const ButtonContainer = styled.div`
+  ${AlignCenter}
+  gap: 12px;
+`;
+
+const TabLink = styled(Link)`
+  font-weight: 600;
+  font-size: var(--font-size-l);
+`;
+
+const Nickname = styled.span`
+  font-weight: 700;
+  font-size: var(--font-size-l);
+`;
+
+const DotIcon = styled(BsDot)`
+  font-size: 18px;
+  color: #bdbdbd;
+`;
+
+const AlarmIcon = styled(BsBell)`
+  font-size: 22px;
+  color: var(--color-primary-pink);
+`;
+
+const UserIcon = styled(BsPersonCircle)`
+  font-size: 24px;
+`;
+
+export default Header;
