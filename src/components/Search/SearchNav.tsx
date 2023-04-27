99d662f6fee8e1bbf01339499d18ebf5140aa08e
youa7878 <dkgus7123@naver.com>
2023-04-27 15:44:08 +0900

    ✨ SearchNav 구현 - #77

---
+++ src/components/Search/SearchNav.tsx
@@ -0,0 +1,63 @@
+import React from 'react';
+import styled from 'styled-components';
+import { Flex } from '../../styles/shared';
+
+interface SearchNavProps {
+  currentTap: string;
+  setCurrentTap: React.Dispatch<React.SetStateAction<string>>;
+}
+
+function SearchNav({ currentTap, setCurrentTap }: SearchNavProps) {
+  return (
+    <SearchNavContainer>
+      <NavList>
+        <NavItem
+          isActive={currentTap === 'Search All'}
+          onClick={() => setCurrentTap('Search All')}
+        >
+          통합검색
+        </NavItem>
+        <NavItem
+          isActive={currentTap === 'BookDiscussion'}
+          onClick={() => setCurrentTap('BookDiscussion')}
+        >
+          독서토론
+        </NavItem>
+        <NavItem
+          isActive={currentTap === 'ProConDiscussion'}
+          onClick={() => setCurrentTap('ProConDiscussion')}
+        >
+          찬반토론
+        </NavItem>
+      </NavList>
+    </SearchNavContainer>
+  );
+}
+
+const SearchNavContainer = styled.nav`
+  ${Flex}
+  height: 80px;
+  background-color: var(--color-subtitle-bg-color);
+  border-bottom: 1px solid var(--color-borderbottom-color);
+`;
+
+const NavList = styled.ul`
+  ${Flex}
+  width: 100%;
+  max-width: 1120px;
+  justify-content: flex-start;
+`;
+
+const NavItem = styled.li<{ isActive: boolean }>`
+  cursor: pointer;
+  padding: 0 20px;
+  font-weight: 700;
+  color: ${({ isActive }) =>
+    isActive ? 'var(--color-primary-pink)' : 'var(--black)'};
+
+  &:hover {
+    opacity: 0.8;
+  }
+`;
+
+export default SearchNav;
