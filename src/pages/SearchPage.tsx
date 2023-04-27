9ff3410054b7fc5ba75c260dfbcf1eda2e132304
youa7878 <dkgus7123@naver.com>
2023-04-27 21:56:25 +0900

    🔨 Tap 별로 조건 랜더링 - #77

--- src/pages/SearchPage.tsx
+++ src/pages/SearchPage.tsx
@@ -1,22 +1,34 @@
 import { useState } from 'react';
 import styled from 'styled-components';
 import MainContainer from '../styles/layout';
-import { BookDiscussionInfo, ProConDiscussionInfo } from '../types';
 import SearchNav from '../components/Search/SearchNav';
 import SubtitleSection from '../components/Search/SubtitleSection';
+import BookDiscussionTap from '../components/Search/BookDiscussionTap';
+import ProConDiscussionTap from '../components/Search/ProConDiscussionTap';
 import BookDiscussionCard from '../components/BookDiscussion/BookDiscussionCard';
 import ProConDiscussionSearchCard from '../components/Search/ProConDiscussionSearchCard';
-import { Flex, bookDiscussionCardContainerCSS } from '../styles/shared';
+
+import { Flex, discussionCardContainerCSS } from '../styles/shared';
+import { BookDiscussionInfo, ProConDiscussionInfo, PageInfo } from '../types';
 
 import BOOKDISCUSSION_DUMMY from '../components/BookDiscussion/BOOKDISCUSSION_DUMMY';
 import PROCONDISCUSSION_DUMMY from '../components/ProConDiscussion/PROCONDISCUSSION_DUMMY';
 
+interface BookDiscussionData {
+  pageInfo: PageInfo;
+  data: BookDiscussionInfo[];
+}
+
+interface ProConDiscussionData {
+  pageInfo: PageInfo;
+  posts: ProConDiscussionInfo[];
+}
+
 function SearchPage() {
-  const [bookDiscussionPosts] = useState<BookDiscussionInfo[]>(
-    BOOKDISCUSSION_DUMMY.data,
-  );
-  const [proConDiscussionPosts] = useState<ProConDiscussionInfo[]>(
-    PROCONDISCUSSION_DUMMY.posts,
+  const [bookDiscussionPosts] =
+    useState<BookDiscussionData>(BOOKDISCUSSION_DUMMY);
+  const [proConDiscussionPosts] = useState<ProConDiscussionData>(
+    PROCONDISCUSSION_DUMMY,
   );
   const [currentTap, setCurrentTap] = useState('Search All');
 
@@ -24,32 +36,52 @@ function SearchPage() {
     <>
       <SearchNav currentTap={currentTap} setCurrentTap={setCurrentTap} />
       <MainContainer>
-        <SubtitleSection
-          subtitle="독서토론"
-          postCount={1234}
-          currentTap={currentTap}
-          setCurrentTap={setCurrentTap}
-        />
-        <BookDiscussionCardContainer>
-          {bookDiscussionPosts.slice(0, 3).map((post) => (
-            <BookDiscussionCard bookDiscussionData={post} key={post.id} />
-          ))}
-        </BookDiscussionCardContainer>
-        <Divider />
-        <SubtitleSection
-          subtitle="찬반토론"
-          postCount={4321}
-          currentTap={currentTap}
-          setCurrentTap={setCurrentTap}
-        />
-        <ProConDiscussionSearchCardContainer>
-          {proConDiscussionPosts.slice(0, 3).map((post) => (
-            <ProConDiscussionSearchCard
-              procondiscussionData={post}
-              key={post.id}
+        {currentTap === 'Search All' && (
+          <>
+            <SubtitleSection
+              subtitle="독서토론"
+              postCount={1234}
+              currentTap={currentTap}
+              setCurrentTap={setCurrentTap}
             />
-          ))}
-        </ProConDiscussionSearchCardContainer>
+            <BookDiscussionCardContainer>
+              {bookDiscussionPosts.data.slice(0, 3).map((post) => (
+                <BookDiscussionCard bookDiscussionData={post} key={post.id} />
+              ))}
+            </BookDiscussionCardContainer>
+            <Divider />
+            <SubtitleSection
+              subtitle="찬반토론"
+              postCount={4321}
+              currentTap={currentTap}
+              setCurrentTap={setCurrentTap}
+            />
+            <ProConDiscussionSearchCardContainer>
+              {proConDiscussionPosts.posts.slice(0, 3).map((post) => (
+                <ProConDiscussionSearchCard
+                  procondiscussionData={post}
+                  key={post.id}
+                />
+              ))}
+            </ProConDiscussionSearchCardContainer>
+          </>
+        )}
+        {currentTap === 'BookDiscussion' && (
+          <BookDiscussionTap
+            posts={bookDiscussionPosts.data}
+            paginationInfo={bookDiscussionPosts.pageInfo}
+            currentTap={currentTap}
+            setCurrentTap={setCurrentTap}
+          />
+        )}
+        {currentTap === 'ProConDiscussion' && (
+          <ProConDiscussionTap
+            posts={proConDiscussionPosts.posts}
+            paginationInfo={proConDiscussionPosts.pageInfo}
+            currentTap={currentTap}
+            setCurrentTap={setCurrentTap}
+          />
+        )}
       </MainContainer>
     </>
   );
@@ -65,7 +97,7 @@ const Divider = styled.hr`
 `;
 
 const BookDiscussionCardContainer = styled.section`
-  ${bookDiscussionCardContainerCSS}
+  ${discussionCardContainerCSS}
 `;
 
 export default SearchPage;
