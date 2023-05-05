1e23dfbbd3e57ae62624f6dcdf29e2b6505037d2
youa7878 <dkgus7123@naver.com>
2023-05-06 00:34:24 +0900

    ✨ proxy 설정 추가 - #101

--- vite.config.ts
+++ vite.config.ts
@@ -5,4 +5,15 @@ import svgr from 'vite-plugin-svgr';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), svgr()],
+  server: {
+    proxy: {
+      '/api': {
+        target: 'http://www.aladin.co.kr/ttb/api',
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, ''),
+        secure: false,
+        ws: true,
+      },
+    },
+  },
 });
