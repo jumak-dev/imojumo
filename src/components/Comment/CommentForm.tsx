fdfaca3f6f1781cb032ecca7c9d1ea5dcb45a45f
Paksubeen <tnqls520@gmail.com>
2023-05-09 23:45:07 +0900

    🔨 댓글 작성 폼 로그인 분기 추가 - #97

--- src/components/Comment/CommentForm.tsx
+++ src/components/Comment/CommentForm.tsx
@@ -1,14 +1,18 @@
+import { useRecoilValue } from 'recoil';
 import styled from 'styled-components';
 import Input from '../UI/Input/Input';
 import { ButtonBox } from '../UI/Button/Button';
 import { rowFlex } from '../../styles/shared';
 import useInputs from '../../hooks/useInputs';
+import isLoginSelector from '../../recoil/seletors';
 
 interface CommentFormProps {
   onSubmit: (content: string) => void;
 }
 
 function CommentForm({ onSubmit }: CommentFormProps) {
+  const isLogin = useRecoilValue(isLoginSelector);
+
   const [{ content }, onChange, reset] = useInputs({ content: '' });
 
   const handleSubmit = (event: MouseEvent) => {
@@ -23,13 +27,15 @@ function CommentForm({ onSubmit }: CommentFormProps) {
         name="content"
         value={content}
         onChange={onChange}
-        placeholder="댓글을 입력하세요..."
+        disabled={!isLogin}
+        placeholder={isLogin ? '댓글을 입력하세요...' : '로그인이 필요합니다.'}
       />
       <SubmitButton
         type="submit"
         buttonType="buttonRight"
         buttonColor="white"
         buttonSize="sm"
+        disabled={!isLogin}
         onClick={handleSubmit}
       >
         등록
@@ -53,6 +59,11 @@ const SubmitButton = styled(ButtonBox)`
   width: 75px;
   height: 45px;
   color: var(--black);
+
+  &:disabled {
+    cursor: default;
+    pointer-events: none;
+  }
 `;
 
 export default CommentForm;
