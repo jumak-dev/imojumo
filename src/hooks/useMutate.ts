26500f4777d97857edc24a5787b279bf0a5e41b7
Jung Su <wjdwjdtn92@naver.com>
2023-05-09 13:36:39 +0900

    ✨ get 이외 요청 커스텀 훅 useMutate 추가 - #110

---
+++ src/hooks/useMutate.ts
@@ -0,0 +1,52 @@
+import { useState } from 'react';
+
+interface UseMuateProps<I, T> {
+  fetchFn: (args: I) => Promise<T>;
+  isSuspense?: boolean;
+  isErrorBoundary?: boolean;
+}
+
+function useMuate<I, T>({
+  fetchFn,
+  isSuspense = false,
+  isErrorBoundary = false,
+}: UseMuateProps<I, T>) {
+  const [promise, setPromise] = useState<Promise<void>>();
+  const [status, setStatus] = useState<'pending' | 'fulfilled' | 'error'>(
+    'pending',
+  );
+  const [result, setResult] = useState<T>();
+  const [error, setError] = useState<Error>();
+
+  const resolvePromise = (promiseResult: T) => {
+    setStatus('fulfilled');
+    setResult(promiseResult);
+  };
+
+  const rejectPromise = (promiseError: Error) => {
+    setStatus('error');
+    setError(promiseError);
+  };
+
+  const mutate = async (arg: I) => {
+    setStatus('pending');
+    setPromise(fetchFn(arg).then(resolvePromise, rejectPromise));
+  };
+
+  if (isSuspense && status === 'pending' && promise) {
+    throw promise;
+  }
+
+  if (isErrorBoundary && status === 'error') {
+    throw error;
+  }
+
+  return {
+    isLoading: status === 'pending',
+    error,
+    data: result,
+    mutate,
+  };
+}
+
+export default useMuate;
