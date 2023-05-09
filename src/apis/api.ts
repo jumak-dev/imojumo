cfcff02f4b505a5cbdc4b6dfca5d21decb5ff180
Jung Su <wjdwjdtn92@naver.com>
2023-05-09 13:31:46 +0900

    ✨ request 패칭 함수 추가 - #110

---
+++ src/apis/api.ts
@@ -0,0 +1,28 @@
+interface APIError {
+  status: number;
+  error: string;
+  message: string[];
+}
+
+interface requestType {
+  url: string;
+  options?: any;
+}
+
+async function request({ url, options }: requestType): Promise<any> {
+  const response = await fetch(url, options);
+  const responseJson = await response.json();
+
+  if (!response.ok) {
+    const error: APIError = {
+      status: responseJson.statusCode,
+      error: responseJson.error,
+      message: responseJson.message,
+    };
+    throw error;
+  }
+
+  return responseJson;
+}
+
+export default request;
