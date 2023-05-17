50a9a503103a1a83106c256bfaf5ce6c90ec54eb
Jung Su <wjdwjdtn92@naver.com>
2023-05-17 13:55:06 +0900

    🔨 onSuccess, onError, onSettled 파리미터 추가 - #110

--- src/hooks/useMutate.ts
+++ src/hooks/useMutate.ts
@@ -4,12 +4,18 @@ interface UseMuateProps<I, T> {
   fetchFn: (args: I) => Promise<T>;
   isSuspense?: boolean;
   isErrorBoundary?: boolean;
+  onSuccess?: (data: T | null) => void;
+  onError?: (error: unknown) => void;
+  onSettled?: (data: T | null, error: unknown) => void;
 }
 
 function useMutate<I, T>({
   fetchFn,
   isSuspense = false,
   isErrorBoundary = false,
+  onSuccess,
+  onError,
+  onSettled,
 }: UseMuateProps<I, T>) {
   const [promise, setPromise] = useState<Promise<void>>();
   const [status, setStatus] = useState<'pending' | 'fulfilled' | 'error'>(
@@ -21,11 +27,27 @@ function useMutate<I, T>({
   const resolvePromise = (promiseResult: T) => {
     setStatus('fulfilled');
     setResult(promiseResult);
+
+    if (onSuccess && typeof onSuccess === 'function') {
+      onSuccess(promiseResult);
+    }
+
+    if (onSettled && typeof onSettled === 'function') {
+      onSettled(promiseResult, null);
+    }
   };
 
   const rejectPromise = (promiseError: Error) => {
     setStatus('error');
     setError(promiseError);
+
+    if (onError && typeof onError === 'function') {
+      onError(promiseError);
+    }
+
+    if (onSettled && typeof onSettled === 'function') {
+      onSettled(null, promiseError);
+    }
   };
 
   const mutate = async (arg: I) => {
