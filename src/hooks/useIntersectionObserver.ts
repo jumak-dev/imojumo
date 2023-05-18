d4428faa24a983536c5131ac5fcfabcb2cce9f72
Jung Su <wjdwjdtn92@naver.com>
2023-05-19 03:41:50 +0900

    ✨ useIntersectionObserver 추가 - #121

---
+++ src/hooks/useIntersectionObserver.ts
@@ -0,0 +1,37 @@
+import { useEffect, useState } from 'react';
+
+interface IntersectionObserverCallback {
+  (entries: IntersectionObserverEntry[], observer: IntersectionObserver): void;
+}
+
+type useIntersectionObserverProps = {
+  root?: null;
+  rootMargin?: string;
+  threshold?: number;
+  onIntersect: IntersectionObserverCallback;
+};
+
+const useIntersectionObserver = ({
+  root,
+  rootMargin = '0px',
+  threshold = 1,
+  onIntersect,
+}: useIntersectionObserverProps) => {
+  const [target, setTarget] = useState<HTMLElement | null | undefined>(null);
+
+  useEffect(() => {
+    if (!target) return undefined;
+
+    const observer: IntersectionObserver = new IntersectionObserver(
+      onIntersect,
+      { root, rootMargin, threshold },
+    );
+    observer.observe(target);
+
+    return () => observer.unobserve(target);
+  }, [onIntersect, root, rootMargin, target, threshold]);
+
+  return { setTarget };
+};
+
+export default useIntersectionObserver;
