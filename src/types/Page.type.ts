c2cce46025854a9a26e5ac0c33ee1176864811cc
youa7878 <dkgus7123@naver.com>
2023-05-18 19:54:07 +0900

    🔨 PaginationType 추가 - #7

--- src/types/Page.type.ts
+++ src/types/Page.type.ts
@@ -1,6 +1,14 @@
+import React from 'react';
+
 export interface PageInfo {
   page: number;
   totalPage: number;
   totalCount: number;
   currentCount: number;
+}
+
+export interface PaginationType {
+  pageInfo: PageInfo;
+  currentPage: number;
+  setPaginate: React.Dispatch<React.SetStateAction<number>>;
 }
