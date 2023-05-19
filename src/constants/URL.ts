eee998c2f66c8378f84a2633054517d8623340d4
youa7878 <dkgus7123@naver.com>
2023-05-19 22:46:41 +0900

    🔨 도서 기본 이미지 상수 추가 - #101

--- src/constants/URL.ts
+++ src/constants/URL.ts
@@ -1,6 +1,8 @@
 const URL = {
   DEFAULT_AVATA_URL:
     'https://www.thechooeok.com/common/img/default_profile.png',
+  DEFAULT_BOOK_IMAGE_URL:
+    'https://image.aladin.co.kr/product/28448/6/cover500/k212835618_2.jpg',
 } as const;
 
 export default URL;
