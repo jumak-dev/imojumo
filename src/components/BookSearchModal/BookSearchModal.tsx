6832f0cb3a6f9364c0b5ba4266d156a79ddea172
Jung Su <wjdwjdtn92@naver.com>
2023-05-19 20:01:58 +0900

    🔨 검색 결과 모달 무한스크롤 적용 - #111

--- src/components/BookSearchModal/BookSearchModal.tsx
+++ src/components/BookSearchModal/BookSearchModal.tsx
@@ -1,42 +1,80 @@
-import React from 'react';
+import React, { useRef } from 'react';
 import styled from 'styled-components';
 
+import { AiOutlineEllipsis } from 'react-icons/ai';
 import NonContentModal from '../UI/NonContentModal/NonContentModal';
 import BookSearchList from './BookSearchList';
 import BookSearchListItem from './BookSearchListItem';
 
-import BOOK_DUMY from './BOOK_DUMY';
-import { screenReaderTextCSS } from '../../styles/shared';
+import { flex, screenReaderTextCSS } from '../../styles/shared';
+import useSearchAladinBook from '../../hooks/aladin/useAladinBookInfiniteQuery';
+import useOnClickOutside from '../../hooks/useOnClickOutside';
+import { AladinBookSearchItem } from '../../types';
+import useIntersectionObserver from '../../hooks/useIntersectionObserver';
+import Loading from '../UI/Loading/Loading';
 
 interface BookSearchModalProps {
+  query: string;
+  onClick: (book: AladinBookSearchItem) => void;
   onClose: () => void;
-  showModal: boolean;
 }
 
-function BookSearchModal({ onClose, showModal }: BookSearchModalProps) {
-  const handleItemClick = () => {
-    onClose();
-  };
+function BookSearchModal({ query, onClick, onClose }: BookSearchModalProps) {
+  if (query.length === 0) {
+    return null;
+  }
+  const bookSearchModalCardRef = useRef<HTMLUListElement>(null);
+  const {
+    data: searchPages,
+    isLoading: isSearchLoading,
+    fetchNextPage,
+    hasNextPage,
+    isNoResult,
+  } = useSearchAladinBook({
+    query,
+    parameter: 'ItemSearch.aspx',
+  });
+  const { setTarget } = useIntersectionObserver({
+    onIntersect: ([{ isIntersecting }]) => {
+      if (isIntersecting) fetchNextPage();
+    },
+  });
+
+  useOnClickOutside(bookSearchModalCardRef, onClose);
+  console.log(hasNextPage);
 
   return (
-    <BookSearchModalCard showModal={showModal}>
+    <BookSearchModalCard ref={bookSearchModalCardRef}>
       <BookSearchModlaTitle>인터파크 도서 검색 결과 모달</BookSearchModlaTitle>
+      {isSearchLoading && !isNoResult && (
+        <NoneContentText>
+          <Loading />
+        </NoneContentText>
+      )}
+      {isNoResult && <NoneContentText>조회 결과없습니다</NoneContentText>}
       <BookSearchList>
-        {BOOK_DUMY.map((book) => (
-          <BookSearchListItem
-            key={book.isbn}
-            book={book}
-            onClick={handleItemClick}
-          />
-        ))}
+        {searchPages?.map((page) =>
+          page?.item?.map((book) => (
+            <BookSearchListItem
+              key={book.isbn13}
+              book={book}
+              onClick={onClick}
+            />
+          )),
+        )}
       </BookSearchList>
+      {!isSearchLoading && hasNextPage && (
+        <EllipsisIconContainer ref={setTarget}>
+          <EllipsisIcon />
+        </EllipsisIconContainer>
+      )}
     </BookSearchModalCard>
   );
 }
 
 const BookSearchModalCard = styled(NonContentModal)`
-  max-width: 970px;
-  max-height: 655px;
+  width: 970px;
+  height: 655px;
   background-color: var(--white);
 
   && {
@@ -52,6 +90,25 @@ const BookSearchModalCard = styled(NonContentModal)`
 
 const BookSearchModlaTitle = styled.h2`
   ${screenReaderTextCSS}
+`;
+
+const NoneContentText = styled.div`
+  ${flex};
+  width: 100%;
+  height: 655px;
+  text-align: center;
+`;
+
+const EllipsisIconContainer = styled.span`
+  display: block;
+  width: fit-content;
+  margin: 0 auto;
+  font-size: 40px;
+  color: #9f9f9f;
+`;
+
+const EllipsisIcon = styled(AiOutlineEllipsis)`
+  font-weight: 900;
 `;
 
 export default BookSearchModal;
