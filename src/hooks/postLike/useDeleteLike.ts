2ab992b34a6fe752d2506400d8c94e42e66eb6df
youa7878 <dkgus7123@naver.com>
2023-05-21 01:12:53 +0900

    ✨ useDeleteLike hooks 추가 - #100

---
+++ src/hooks/postLike/useDeleteLike.ts
@@ -0,0 +1,15 @@
+import { deleteLike } from '../../apis/imojumo/postLike';
+import { UsePostLikeType, PostLikeResponse } from '../../types';
+import useMutate from '../useMutate';
+
+function useCreateLike({ onSuccess, onError }: UsePostLikeType) {
+  const { mutate, data, error, isLoading } = useMutate<any, PostLikeResponse>({
+    fetchFn: deleteLike,
+    onSuccess,
+    onError,
+  });
+
+  return { mutate, data, error, isLoading };
+}
+
+export default useCreateLike;
