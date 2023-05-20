a966f329cfb24f492ac1b9fdaf7779715e172a99
youa7878 <dkgus7123@naver.com>
2023-05-20 23:49:51 +0900

    🔨 독서토론 게시물 get hooks 적용 - #101

--- src/pages/BookDiscussionPage.tsx
+++ src/pages/BookDiscussionPage.tsx
@@ -1,65 +1,35 @@
-import { useState, useEffect } from 'react';
+import { useState } from 'react';
 import styled from 'styled-components';
 import MainContainer from '../styles/layout';
-import { BookDiscussionInfo, PageInfo, GetBookDiscussion } from '../types';
 import { discussionCardContainerCSS } from '../styles/shared';
 import Pagination from '../components/UI/Pagination/Pagination';
+import useBookDiscussion from '../hooks/bookDiscussion/useBookDisscussion';
 import BookDiscussionCard from '../components/BookDiscussion/BookDiscussionCard';
 
 function BookDiscussion() {
-  const { VITE_API_URL } = import.meta.env;
-  const [posts, setPosts] = useState<BookDiscussionInfo[]>([]);
   const [paginate, setPaginate] = useState(1);
-  const [pageInfo, setPageInfo] = useState<PageInfo>({
-    page: 1,
-    totalPage: 1,
-    totalCount: 1,
-    currentCount: 1,
+
+  const { data: bookDiscussion } = useBookDiscussion({
+    page: paginate || 1,
+    limit: 9,
   });
-
-  // apis로 뺄 예정
-  const getBookDiscussion = async (
-    page: number,
-  ): Promise<GetBookDiscussion> => {
-    const url = `${VITE_API_URL}/book-discussions?page=${page}&limit=9`;
-
-    const response = await fetch(url, {
-      method: 'GET',
-      headers: {
-        'Content-Type': 'application/json',
-        'ngrok-skip-browser-warning': '12',
-      },
-    });
-
-    const data = await response.json();
-    return data;
-  };
-
-  useEffect(() => {
-    try {
-      getBookDiscussion(paginate).then((res) => {
-        setPosts(res.posts);
-        setPageInfo(res.pageInfo);
-      });
-    } catch (e) {
-      console.log(e);
-    }
-  }, [paginate]);
 
   return (
     <MainContainer>
       <Subtitle>독서토론</Subtitle>
       <BookDiscussionCardContainer>
-        {posts &&
-          posts.map((post) => (
+        {bookDiscussion &&
+          bookDiscussion.posts.map((post) => (
             <BookDiscussionCard bookDiscussionData={post} key={post.id} />
           ))}
       </BookDiscussionCardContainer>
-      <Pagination
-        currentPage={paginate}
-        setPaginate={setPaginate}
-        pageInfo={pageInfo}
-      />
+      {bookDiscussion && (
+        <Pagination
+          currentPage={paginate}
+          setPaginate={setPaginate}
+          pageInfo={bookDiscussion?.pageInfo}
+        />
+      )}
     </MainContainer>
   );
 }
@@ -72,6 +42,7 @@ export const Subtitle = styled.h2`
 
 const BookDiscussionCardContainer = styled.section`
   ${discussionCardContainerCSS}
+  margin: 0 50px 50px;
 `;
 
 export default BookDiscussion;
