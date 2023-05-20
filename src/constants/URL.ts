80dbb2d2f2bf80f91ea662d8a60123443696aea6
youa7878 <dkgus7123@naver.com>
2023-05-20 15:34:28 +0900

    🔨 NONE_AVATA_URL 상수 추가 - #101

--- src/constants/URL.ts
+++ src/constants/URL.ts
@@ -1,6 +1,8 @@
 const URL = {
   DEFAULT_AVATA_URL:
     'https://www.thechooeok.com/common/img/default_profile.png',
+  NONE_AVATA_URL:
+    'https://www.pngitem.com/pimgs/m/80-806189_red-x-circle-icon-hd-png-download.png',
   DEFAULT_BOOK_IMAGE_URL:
     'https://image.aladin.co.kr/product/28448/6/cover500/k212835618_2.jpg',
 } as const;
