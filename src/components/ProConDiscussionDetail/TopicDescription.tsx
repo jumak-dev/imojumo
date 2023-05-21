520c912bea3b6a25a4441d9d80332290abfec6d3
Paksubeen <tnqls520@gmail.com>
2023-05-21 17:34:43 +0900

    🔨 찬반토론 투표 이벤트 처리 - #119

--- src/components/ProConDiscussionDetail/TopicDescription.tsx
+++ src/components/ProConDiscussionDetail/TopicDescription.tsx
@@ -1,26 +1,53 @@
-import styled from 'styled-components';
+import { Dispatch, SetStateAction } from 'react';
+import { useNavigate } from 'react-router-dom';
+import { useRecoilValue } from 'recoil';
+import styled, { css } from 'styled-components';
 import ButtonBox from '../UI/Button/Button';
+import isLoginSelector from '../../recoil/seletors';
 
-function TopicDescription() {
+interface TopicDescriptionProps {
+  content: string;
+  isPro: boolean;
+  isVote: boolean;
+  setIsPro: Dispatch<SetStateAction<boolean>>;
+  setIsVote: Dispatch<SetStateAction<boolean>>;
+}
+
+function TopicDescription({
+  content,
+  isPro,
+  isVote,
+  setIsPro,
+  setIsVote,
+}: TopicDescriptionProps) {
+  const navigate = useNavigate();
+  const isLogin = useRecoilValue(isLoginSelector);
+
+  const handleVote = (voteValue: boolean) => {
+    if (!isLogin) {
+      navigate('/login');
+      return;
+    }
+
+    if (!isVote) {
+      setIsVote(true);
+    }
+
+    setIsPro(voteValue);
+  };
+
   return (
     <DescriptionContainer>
-      <DescriptionText>
-        5억년 버튼을 누르면 아무것도 없는 광활하고 어두운 빈 공간에 5억년동안
-        갇힌 채 1분 1초를 생생히 느끼며 지내야 합니다. 잠을 잘수도, 뭔가를
-        먹을수도 없지만 그로 인한 정신적, 신체적 스트레스는 없습니다. 5억년간
-        신체는 건강하게 유지됩니다. 5억년이 지나면 버튼을 누른 직후로 돌아옴과
-        동시에 5억년간의 기억이 전부 사라지며 100억원을 받습니다. 실제로 5억년의
-        시간을 지낸 후이지만 당신은 버튼을 누르고 바로 100억원이 당신의 손에
-        쥐어지는 것처럼 느낍니다. 옆의 당신의 친구는 벌써 버튼을 눌러 즉시
-        지급된 100억원을 얻고 기뻐합니다. 당신은 이 설명을 듣고도 5억년 버튼을
-        누르시겠습니까?
-      </DescriptionText>
+      <DescriptionText>{content}</DescriptionText>
       <ButtonContainer>
         <ProConButton
           type="button"
           buttonType="buttonLeft"
           buttonColor="mint"
           buttonSize="l"
+          isVote={isVote}
+          isActive={isPro}
+          onClick={() => handleVote(true)}
         >
           찬성
         </ProConButton>
@@ -29,6 +56,9 @@ function TopicDescription() {
           buttonType="buttonRight"
           buttonColor="pink"
           buttonSize="l"
+          isVote={isVote}
+          isActive={!isPro}
+          onClick={() => handleVote(false)}
         >
           반대
         </ProConButton>
@@ -54,9 +84,21 @@ const ButtonContainer = styled.div`
   justify-content: center;
 `;
 
-const ProConButton = styled(ButtonBox)`
+const buttonActiveCSS = css`
+  pointer-events: none;
+`;
+
+const buttonDeactivateCSS = css`
+  filter: brightness(75%);
+`;
+
+const ProConButton = styled(ButtonBox)<{ isVote: boolean; isActive: boolean }>`
   width: 80px;
   height: 40px;
+  transition: all 0.5s cubic-bezier(0.25, 0.8, 0.25, 1);
+
+  ${({ isVote, isActive }) =>
+    isVote && (isActive ? buttonActiveCSS : buttonDeactivateCSS)}
 `;
 
 export default TopicDescription;
