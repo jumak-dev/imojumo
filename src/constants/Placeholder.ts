fc9922bfbf2bae39db10238243cb3b29972bb728
Paksubeen <tnqls520@gmail.com>
2023-05-21 17:21:58 +0900

    ✨ 댓글 작성 폼 Placeholder 상수 추가 - #119

---
+++ src/constants/Placeholder.ts
@@ -0,0 +1,8 @@
+const PLACEHOLDER = {
+  LOGGED_IN_WITH_VOTE: '의견을 자유롭게 작성해주세요!',
+  LOGGED_IN_WITHOUT_VOTE: '투표 후에 댓글을 작성할 수 있습니다.',
+  NOT_LOGGED_IN: '로그인이 필요합니다.',
+  LOGGED_IN: '댓글을 입력하세요...',
+} as const;
+
+export default PLACEHOLDER;
