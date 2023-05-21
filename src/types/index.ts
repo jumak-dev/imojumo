855ec3aeb094a697ba0beb933d46dda93e16dc6f
Paksubeen <tnqls520@gmail.com>
2023-05-22 00:31:53 +0900

    🔨 BookDetail 타입 추가 - #139

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,5 +1,6 @@
 export * from './Book.type';
 export * from './Page.type';
+export * from './BookDetail.type';
 export * from './BookDiscussionPost.type';
 export * from './UseSearchAladinBook.type';
 export * from './ProConDiscussionPost.type';
