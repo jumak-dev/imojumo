3bee352699fcb5e0c279fdad612edb45576a466d
Jung Su <wjdwjdtn92@naver.com>
2023-05-22 01:52:23 +0900

    🔨 handleUpdateLike  추가 - #124

--- src/hooks/searchDiscussion/useSearchDiscussion.ts
+++ src/hooks/searchDiscussion/useSearchDiscussion.ts
@@ -16,22 +16,52 @@ function useSearchDiscussion({
   page,
   limit,
   type,
+  token,
   delay = 1500,
   isSuspense = false,
   isErrorBoundary = false,
 }: UseSearchDiscussionType) {
-  const { data, isLoading, error } = useQuery<
+  const { data, isLoading, error, setData } = useQuery<
     any,
     SearchDiscussionsResponseType
   >({
     fetchFn: searchDiscussions,
-    arg: { query, page, limit, type },
+    arg: { query, page, limit, type, token },
     isErrorBoundary,
     isSuspense,
     delay,
   });
 
-  return { data, isLoading, error };
+  const handleUpdateLike = (postId: number, likeCount: number) => {
+    if (!setData) {
+      return;
+    }
+
+    setData((prev) => {
+      if (prev) {
+        const updatedPosts = prev.bookResults.posts.map((post) => {
+          if (post.id === postId) {
+            return {
+              ...post,
+              likeCount,
+            };
+          }
+          return post;
+        });
+
+        return {
+          ...prev,
+          bookResults: {
+            ...prev.bookResults,
+            posts: updatedPosts,
+          },
+        };
+      }
+      return prev;
+    });
+  };
+
+  return { data, isLoading, error, setData, handleUpdateLike };
 }
 
 export default useSearchDiscussion;
