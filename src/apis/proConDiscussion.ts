5c4d8e15e1c42f5e51df1126ae1571b8b6ca6c11
Paksubeen <tnqls520@gmail.com>
2023-05-21 17:56:12 +0900

    🔨 찬반투표 API 요청 추가 - #119

--- src/apis/proConDiscussion.ts
+++ src/apis/proConDiscussion.ts
@@ -8,6 +8,10 @@ interface ProConDiscussion extends ProConDiscussionInfo {
   comments: Comment[];
 }
 
+interface ProConVote {
+  isPro: boolean;
+}
+
 export async function getProConDiscussion(
   id: string,
   token: string,
@@ -49,6 +53,60 @@ export async function deleteProConDiscussion(id: number, token: string) {
     }
   } catch (error) {
     console.log(error);
+    throw error;
+  }
+}
+
+export async function createProConVote(
+  id: string,
+  token: string,
+  voteValue: boolean,
+): Promise<ProConVote> {
+  try {
+    const response = await fetch(`${VITE_API_URL}/pro-con-vote/${id}`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        Authorization: `${token}`,
+      },
+      body: JSON.stringify({ isPro: voteValue }),
+    });
+
+    if (!response.ok) {
+      const error = await response.json();
+      throw new Error(error.message);
+    }
+
+    return (await response.json()) as ProConVote;
+  } catch (error) {
+    console.error(error);
+    throw error;
+  }
+}
+
+export async function updateProConVote(
+  id: string,
+  token: string,
+  voteValue: boolean,
+): Promise<ProConVote> {
+  try {
+    const response = await fetch(`${VITE_API_URL}/pro-con-vote/${id}`, {
+      method: 'PATCH',
+      headers: {
+        'Content-Type': 'application/json',
+        Authorization: `${token}`,
+      },
+      body: JSON.stringify({ isPro: voteValue }),
+    });
+
+    if (!response.ok) {
+      const error = await response.json();
+      throw new Error(error.message);
+    }
+
+    return (await response.json()) as ProConVote;
+  } catch (error) {
+    console.error(error);
     throw error;
   }
 }
