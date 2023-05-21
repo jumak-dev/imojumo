00d7add61a8ad0d393525a9214eb767f675135f4
Paksubeen <tnqls520@gmail.com>
2023-05-21 22:13:02 +0900

    🔨 도서 정보 데이터 처리 - #139

--- src/components/BookDetail/BookInformation.tsx
+++ src/components/BookDetail/BookInformation.tsx
@@ -2,11 +2,14 @@ import styled from 'styled-components';
 import { useNavigate } from 'react-router-dom';
 import { alignCenter, colFlex, rowFlex } from '../../styles/shared';
 import Button from '../UI/Button/Button';
+import { AladinBookSearchItem } from '../../types';
+import replaceHtml from '../../utils/replaceHtml';
 
-function BookInformation() {
-  const imageUrl =
-    'https://image.aladin.co.kr/product/27222/22/cover500/e822538010_1.jpg';
+interface BookInformationProps {
+  bookInfo: AladinBookSearchItem | undefined;
+}
 
+function BookInformation({ bookInfo }: BookInformationProps) {
   const navigate = useNavigate();
 
   const handleClick = () => {
@@ -15,10 +18,10 @@ function BookInformation() {
 
   return (
     <InformationContainer>
-      <BookImage src={imageUrl} alt="미드나잇 라이브러리" />
+      <BookImage src={bookInfo?.cover} alt={bookInfo?.title} />
       <BookInfoContainer>
         <TitleContainer>
-          <BookTitle>미드나잇 라이브러리</BookTitle>
+          <BookTitle>{bookInfo?.title}</BookTitle>
           <Button
             type="button"
             buttonType="button"
@@ -32,38 +35,30 @@ function BookInformation() {
         <BookInfoList>
           <BookInfoItem>
             <InfoLabel>저자</InfoLabel>
-            <InfoText>매트 헤이그</InfoText>
-          </BookInfoItem>
-          <BookInfoItem>
-            <InfoLabel>역자</InfoLabel>
-            <InfoText>노진선</InfoText>
+            <InfoText>{bookInfo?.author}</InfoText>
           </BookInfoItem>
           <BookInfoItem>
             <InfoLabel>출판사</InfoLabel>
-            <InfoText>인플루엔셜</InfoText>
+            <InfoText>{bookInfo?.publisher}</InfoText>
           </BookInfoItem>
           <BookInfoItem>
             <InfoLabel>발행일</InfoLabel>
-            <InfoText>2021년 04년 28일</InfoText>
+            <InfoText>{bookInfo?.pubDate}</InfoText>
           </BookInfoItem>
           <BookInfoItem>
             <InfoLabel>페이지</InfoLabel>
-            <InfoText>408</InfoText>
+            <InfoText>{bookInfo?.subInfo.itemPage}쪽</InfoText>
           </BookInfoItem>
           <BookInfoItem>
             <InfoLabel>정가</InfoLabel>
-            <InfoText>15,800원</InfoText>
+            <InfoText>{bookInfo?.priceStandard.toLocaleString()}원</InfoText>
           </BookInfoItem>
           <BookInfoItem>
             <InfoLabel>설명</InfoLabel>
             <BookDescription>
-              매트 헤이그의 소설 《미드나잇 라이브러리》는 2021년 4월 출간 이후
-              10개월 만에 30만 독자의 마음을 사로잡으며 눈물과 웃음, 가슴 뭉클한
-              감동을 선사하고 있다. 죽기로 결심한 주인공 ‘노라 시드’가 삶과 죽음
-              사이에 존재하는 미스터리한 도서관 ‘미드나잇 라이브러리’에서 눈을
-              뜨며 인생의 두 번째 기회를 얻는 이 소설은, 노라의 가장 완벽한 삶을
-              찾는 모험을 따라가며 ‘살아 있음’과 ‘살아가는 것’에 대한 깊은
-              통찰을 보여준다.
+              {bookInfo?.description.length
+                ? replaceHtml(bookInfo?.description)
+                : '해당 도서의 자세한 내용은 곧 업데이트될 예정입니다.'}
             </BookDescription>
           </BookInfoItem>
         </BookInfoList>
