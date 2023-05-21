d5d7c6aa67cc825b6e96879606ca11d3c8520313
Jung Su <wjdwjdtn92@naver.com>
2023-05-21 14:54:47 +0900

    🔨 AllTab 서버 요청 추가 - #124

--- src/components/Search/AllTab.tsx
+++ src/components/Search/AllTab.tsx
@@ -4,51 +4,71 @@ import { useSearchParams } from 'react-router-dom';
 import BookDiscussionCard from '../BookDiscussion/BookDiscussionCard';
 import ProConDiscussionSearchCard from './ProConDiscussionSearchCard';
 import SubtitleSection from './SubtitleSection';
-import { discussionCardContainerCSS, flex } from '../../styles/shared';
+import {
+  alignCenter,
+  discussionCardContainerCSS,
+  flex,
+} from '../../styles/shared';
 import TAB from '../../constants/Tab';
 import useSearchDiscussion from '../../hooks/searchDiscussion/useSearchDiscussion';
+import EmptySearchResult from './EmptySearchResult';
 
 function AllTab() {
   const [searchParams] = useSearchParams();
-  const query = searchParams.get('query');
+  const query = searchParams.get('query') || '';
 
   const { data } = useSearchDiscussion({
-    query: query || '',
+    query,
     type: 'all',
     page: 1,
     limit: 3,
+    isSuspense: true,
   });
+  const bookResultsCount = data?.bookResults?.pageInfo.totalCount || 0;
+  const proConResultsCount = data?.proConResults?.pageInfo.totalCount || 0;
+  const isEmptySearchResult =
+    data && bookResultsCount === 0 && proConResultsCount === 0;
 
   return (
     <>
-      <SubtitleSection
-        subtitle={TAB.BOOK_DISCUSSION}
-        postCount={data?.bookResults?.pageInfo.totalCount || 0}
-      />
-      <BookDiscussionCardContainer>
-        {data?.bookResults.posts.map((post) => (
-          <BookDiscussionCard bookDiscussionData={post} key={post.id} />
-        ))}
-      </BookDiscussionCardContainer>
-      <Divider />
-      <SubtitleSection
-        subtitle={TAB.PROCON_DISCUSSION}
-        postCount={data?.proConResults.pageInfo.totalCount || 0}
-      />
-      <ProConDiscussionSearchCardContainer>
-        {data?.proConResults.posts.map((post) => (
-          <ProConDiscussionSearchCard
-            procondiscussionData={post}
-            key={post.id}
+      {bookResultsCount > 0 && (
+        <>
+          <SubtitleSection
+            subtitle={TAB.BOOK_DISCUSSION}
+            postCount={bookResultsCount}
           />
-        ))}
-      </ProConDiscussionSearchCardContainer>
+          <BookDiscussionCardContainer>
+            {data?.bookResults.posts.map((post) => (
+              <BookDiscussionCard bookDiscussionData={post} key={post.id} />
+            ))}
+          </BookDiscussionCardContainer>
+          <Divider />
+        </>
+      )}
+
+      {proConResultsCount > 0 && (
+        <>
+          <SubtitleSection
+            subtitle={TAB.PROCON_DISCUSSION}
+            postCount={data?.proConResults.pageInfo.totalCount || 0}
+          />
+          <ProConDiscussionSearchCardContainer>
+            {data?.proConResults.posts.map((post) => (
+              <ProConDiscussionSearchCard
+                procondiscussionData={post}
+                key={post.id}
+              />
+            ))}
+          </ProConDiscussionSearchCardContainer>
+        </>
+      )}
+      {isEmptySearchResult && <EmptySearchResult keyword={query} />}
     </>
   );
 }
 
 const ProConDiscussionSearchCardContainer = styled.section`
-  ${flex}
+  ${alignCenter}
 `;
 
 const Divider = styled.hr`
