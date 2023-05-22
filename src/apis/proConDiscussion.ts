d15e88ff946dbfd44a20db1674ff2003f48f0abd
Jung Su <wjdwjdtn92@naver.com>
2023-05-22 22:29:13 +0900

    ♻️ fetch 직적 사용방식에서 reqeust  공통모듈 적용 - #136

--- src/apis/proConDiscussion.ts
+++ src/apis/proConDiscussion.ts
@@ -1,112 +1,88 @@
-import { ProConDiscussionInfo, Comment } from '../types';
+import {
+  CreateProConVoteType,
+  DeleteProConDiscussionType,
+  GetProConDiscussionDetailType,
+  ProConDiscussion,
+  ProConVote,
+  UpdateProConVoteType,
+} from '../types';
+
+import request from './api';
 
 const { VITE_API_URL } = import.meta.env;
 
-interface ProConDiscussion extends ProConDiscussionInfo {
-  isPro: boolean;
-  isVote: boolean;
-  comments: Comment[];
-}
-
-interface ProConVote {
-  isPro: boolean;
-}
-
-export async function getProConDiscussion(
-  id: string,
-  token: string,
-): Promise<ProConDiscussion> {
-  try {
-    const response = await fetch(`${VITE_API_URL}/pro-con-discussions/${id}`, {
+export async function getProConDiscussionDetail({
+  id,
+  token,
+}: GetProConDiscussionDetailType): Promise<ProConDiscussion> {
+  const response = await request({
+    url: `${VITE_API_URL}/pro-con-discussions/${id}`,
+    options: {
       method: 'GET',
       headers: {
         'Content-Type': 'application/json',
         Authorization: `${token}`,
       },
-    });
+    },
+  });
 
-    if (!response.ok) {
-      const error = await response.json();
-      throw new Error(error.message);
-    }
-
-    return (await response.json()) as ProConDiscussion;
-  } catch (error) {
-    console.log(error);
-    throw error;
-  }
+  return response;
 }
 
-export async function deleteProConDiscussion(id: number, token: string) {
-  try {
-    const response = await fetch(`${VITE_API_URL}/pro-con-discussions/${id}`, {
+export async function deleteProConDiscussion({
+  id,
+  token,
+}: DeleteProConDiscussionType) {
+  const response = await request({
+    url: `${VITE_API_URL}/pro-con-discussions/${id}`,
+    options: {
       method: 'DELETE',
       headers: {
         'Content-Type': 'application/json',
         Authorization: `${token}`,
       },
-    });
+    },
+  });
 
-    if (!response.ok) {
-      const error = await response.json();
-      throw new Error(error.message);
-    }
-  } catch (error) {
-    console.log(error);
-    throw error;
-  }
+  return response;
 }
 
-export async function createProConVote(
-  id: string,
-  token: string,
-  voteValue: boolean,
-): Promise<ProConVote> {
-  try {
-    const response = await fetch(`${VITE_API_URL}/pro-con-vote/${id}`, {
+export async function createProConVote({
+  id,
+  token,
+  voteValue,
+}: CreateProConVoteType): Promise<ProConVote> {
+  const response = await request({
+    url: `${VITE_API_URL}/pro-con-vote/${id}`,
+    options: {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json',
         Authorization: `${token}`,
       },
       body: JSON.stringify({ isPro: voteValue }),
-    });
+    },
+  });
 
-    if (!response.ok) {
-      const error = await response.json();
-      throw new Error(error.message);
-    }
-
-    return (await response.json()) as ProConVote;
-  } catch (error) {
-    console.error(error);
-    throw error;
-  }
+  return response;
 }
 
-export async function updateProConVote(
-  id: string,
-  token: string,
-  voteValue: boolean,
-): Promise<ProConVote> {
-  try {
-    const response = await fetch(`${VITE_API_URL}/pro-con-vote/${id}`, {
+export async function updateProConVote({
+  id,
+  token,
+  voteValue,
+}: UpdateProConVoteType): Promise<ProConVote> {
+  const response = await request({
+    url: `${VITE_API_URL}/pro-con-vote/${id}`,
+    options: {
       method: 'PATCH',
       headers: {
         'Content-Type': 'application/json',
         Authorization: `${token}`,
       },
       body: JSON.stringify({ isPro: voteValue }),
-    });
+    },
+  });
 
-    if (!response.ok) {
-      const error = await response.json();
-      throw new Error(error.message);
-    }
-
-    return (await response.json()) as ProConVote;
-  } catch (error) {
-    console.error(error);
-    throw error;
-  }
+  return response;
 }
