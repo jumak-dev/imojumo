295165a7ac978e2559f468562d48fc338cf85886
LeeHyoGeun-create <dkdkdk99835@gmail.com>
2023-06-02 01:03:29 +0900

    :recycle: type 파일 수정

--- src/types/MyPageModal.type.ts
+++ src/types/MyPageModal.type.ts
@@ -1,5 +1,5 @@
 import React from 'react';
-import { PageInfo } from './Page.type';
+
 import { MyPageModalData } from './MyPage.type';
 
 export interface MyPageModalProps {
@@ -9,5 +9,4 @@ export interface MyPageModalProps {
   handleCloseModal: () => void;
   currentPage: number;
   setPagenate: React.Dispatch<React.SetStateAction<number>>;
-  pageInfo: PageInfo;
 }
