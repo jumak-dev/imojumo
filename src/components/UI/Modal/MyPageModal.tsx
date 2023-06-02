dd0e3c5a701564d1f235e6e32734a45b8532e412
LeeHyoGeun-create <dkdkdk99835@gmail.com>
2023-06-02 15:51:29 +0900

    :lipstick: Loading 위치 조정

--- src/components/UI/Modal/MyPageModal.tsx
+++ src/components/UI/Modal/MyPageModal.tsx
@@ -1,43 +1,57 @@
 import styled from 'styled-components';
+import { useEffect } from 'react';
 import ModalPortal from './ModalPortal';
 import { MyPageModalProps } from '../../../types';
 import ContentList from '../../MyPage/ContentList';
 import Button from '../Button/Button';
 import Pagination from '../Pagination/Pagination';
+import Loading from '../Loading/Loading';
 
 function MyPageModal({
   responseDataObj,
   showModal,
   handleCloseModal,
+  isLoading = false,
   currentPage,
   setPagenate,
 }: MyPageModalProps) {
+  let articles;
+
+  if (responseDataObj && 'comments' in responseDataObj) {
+    articles = responseDataObj.comments;
+  } else if (responseDataObj && 'posts' in responseDataObj) {
+    articles = responseDataObj.posts;
+  } else {
+    articles = null;
+  }
+
   return (
     <MyPageModalContainer>
-      {showModal && responseDataObj && (
+      {showModal && (
         <ModalPortal width="80%" maxWidth="600px" onClose={handleCloseModal}>
-          <ContentList
-            articles={
-              'comments' in responseDataObj
-                ? responseDataObj.comments
-                : responseDataObj.posts
-            }
-          />
-          <MyPagenation
-            currentPage={currentPage}
-            setPaginate={setPagenate}
-            pageInfo={responseDataObj.pageInfo}
-          />
-          <Button
+          {isLoading && !articles ? (
+            <Loading />
+          ) : (
+            <>
+              <ContentList articles={articles} />
+              <MyPagenation
+                currentPage={currentPage}
+                setPaginate={setPagenate}
+                pageInfo={responseDataObj && responseDataObj?.pageInfo}
+              />
+            </>
+          )}
+          <MyPageButton
             type="button"
             buttonType="button"
             name="out"
             buttonColor="mint"
             buttonSize="m"
             onClick={handleCloseModal}
+            Loading={!articles}
           >
             확인
-          </Button>
+          </MyPageButton>
         </ModalPortal>
       )}
     </MyPageModalContainer>
@@ -51,6 +65,10 @@ const MyPagenation = styled(Pagination)`
 const MyPageModalContainer = styled.section`
   width: 50%;
   max-width: 600px;
+`;
+
+const MyPageButton = styled(Button)`
+  margin-top: ${(props) => props.loading && '200px'};
 `;
 
 export default MyPageModal;
