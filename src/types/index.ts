de502d8b23c6dbefba0db030a7bcde1af588872d
LeeHyoGeun-create <dkdkdk99835@gmail.com>
2023-06-02 12:53:53 +0900

    merge

--- src/types/index.ts
+++ src/types/index.ts
@@ -10,8 +10,5 @@ export * from './Promise.type';
 export * from './Error.type';
 export * from './MyPage.type';
 export * from './ProConVote.type';
-<<<<<<< HEAD
 export * from './MyPageModal.type';
-=======
 export * from './Notification.type';
->>>>>>> ac4a6eaca70d378b52b87b6874de73231a84d570
