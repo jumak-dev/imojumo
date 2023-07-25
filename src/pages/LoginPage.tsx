26e931883d7667eed86de90fd82f01b66fb29ba3
Jung Su <wjdwjdtn92@naver.com>
2023-07-25 11:51:30 +0900

    ♻️ 로그인/회원가입 페이지 통합 - #88

--- src/pages/LoginPage.tsx
+++ src/pages/LoginPage.tsx
@@ -2,22 +2,32 @@ import styled from 'styled-components';
 import { useLocation, useNavigate } from 'react-router-dom';
 import { useRecoilValue, useSetRecoilState } from 'recoil';
 import { useEffect, useState } from 'react';
-import FormBox from '../components/LoginSignupForm/Form';
+import { useGoogleLogin } from '@react-oauth/google';
+
+import { APIError } from '../types';
 import MainContainer from '../styles/layout';
+import { alignCenter } from '../styles/shared';
+import FormBox from '../components/LoginSignupForm/Form';
+import WelcomeMessage from '../components/Auth/WelcomeMessage';
 import { userInfoAtom, jwtAtom } from '../recoil/atoms';
 import isLoginSelector from '../recoil/seletors';
-import { alignCenter } from '../styles/shared';
+import { googleLogin } from '../apis/auth';
 import useLogin from '../hooks/auth/useLogin';
-import WelcomeMessage from '../components/Auth/WelcomeMessage';
+import useSignup from '../hooks/auth/useSignup';
+import validate from '../utils/auth/signupValidate';
 
-function LoginPage() {
-  const location = useLocation();
-  const { pathname } = location;
+interface LoginPageProps {
+  authType: 'login' | 'signup';
+}
+
+function LoginPage({ authType }: LoginPageProps) {
   const navigate = useNavigate();
   const setUserInfo = useSetRecoilState(userInfoAtom);
   const setJwt = useSetRecoilState(jwtAtom);
   const isLogin = useRecoilValue(isLoginSelector);
   const [displayError, setDisplayError] = useState('');
+  const isLoginPage = authType === 'login';
+
   const { mutate: loginMutate } = useLogin({
     onSuccess: ({ response, responseJson }) => {
       const jwt = response.headers.get('authorization');
@@ -28,6 +38,53 @@ function LoginPage() {
     onError: (error) => {
       console.log(error);
       setDisplayError(String(error.message));
+    },
+  });
+
+  const { mutate: signupMutate } = useSignup({
+    onSuccess: () => {
+      navigate('/login');
+    },
+    onError: (error) => {
+      console.log(error);
+      setDisplayError(String(error.message));
+    },
+  });
+
+  const handleSignup = async (
+    email: string,
+    password: string,
+    checkPassword: string,
+  ) => {
+    if (validate(email, password, checkPassword, setDisplayError)) {
+      signupMutate({ email, password });
+    }
+  };
+
+  const handleGoogleLogin = useGoogleLogin({
+    flow: 'auth-code',
+    onSuccess: async ({ code }) => {
+      try {
+        const { response, responseJson } = await googleLogin(code);
+        console.log(response);
+        const jwt = response.headers.get('authorization');
+
+        setUserInfo(responseJson);
+        setJwt(jwt);
+        navigate('/');
+      } catch (error) {
+        console.log(error);
+
+        if (typeof error === 'object' && error !== null && 'message' in error) {
+          setDisplayError(String((error as APIError).message));
+        } else {
+          setDisplayError(String(error));
+        }
+      }
+    },
+    onError: (errorResponse) => {
+      console.log(errorResponse.error);
+      setDisplayError(String(errorResponse.error_description));
     },
   });
 
@@ -45,8 +102,9 @@ function LoginPage() {
     <PageContainer>
       <WelcomeMessage />
       <FormBox
-        pathname={pathname}
-        onSubmit={handleLogin}
+        isLoginPage={isLoginPage}
+        onSubmit={isLoginPage ? handleLogin : handleSignup}
+        onGoogleSubmit={handleGoogleLogin}
         displayError={displayError}
       />
     </PageContainer>
