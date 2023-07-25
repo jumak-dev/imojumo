9fc8229a0028cdddf480ae5629a563c0a3cc5710
Jung Su <wjdwjdtn92@naver.com>
2023-07-25 11:49:33 +0900

    🔨 GoogleOAuthProvider 추가 - #88

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,18 @@
 import { RouterProvider } from 'react-router-dom';
 import { RecoilRoot } from 'recoil';
+import { GoogleOAuthProvider } from '@react-oauth/google';
 import LoggedInHandler from './components/LoggedInHandler';
 import route from './Routes';
 
 function App() {
   return (
-    <RecoilRoot>
-      <LoggedInHandler>
-        <RouterProvider router={route} />
-      </LoggedInHandler>
-    </RecoilRoot>
+    <GoogleOAuthProvider clientId="418129859233-1o5p4gnkqdmhgise9i2iv00qioigb2ms.apps.googleusercontent.com">
+      <RecoilRoot>
+        <LoggedInHandler>
+          <RouterProvider router={route} />
+        </LoggedInHandler>
+      </RecoilRoot>
+    </GoogleOAuthProvider>
   );
 }
 
